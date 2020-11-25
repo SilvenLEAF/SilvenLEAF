d681769b5cd6d01f373ed5a689fbc4c6bffa6566
SilvenLEAF <SilvenLEAF@gmail.com>
2020-11-25 08:25:45 +0530

    Updates

--- precache-manifest.0599a06029e387937abadb497dc763b8.js
+++ precache-manifest.0599a06029e387937abadb497dc763b8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1286615a91d4b5338a88e462f9d19df1",
+    "revision": "212d1c5fb9312621528fe29921fd5983",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.e461d6de.chunk.css"
   },
   {
-    "revision": "51646e01879dc77bbc84",
-    "url": "/static/css/main.bfa9f49a.chunk.css"
+    "revision": "3b6c7057b88deefab4da",
+    "url": "/static/css/main.e5133ea0.chunk.css"
   },
   {
     "revision": "956935c7748018e04bf9",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.4877db76.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "51646e01879dc77bbc84",
-    "url": "/static/js/main.7ad7cdba.chunk.js"
+    "revision": "3b6c7057b88deefab4da",
+    "url": "/static/js/main.93beaf52.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d786eacc3978039ebba0cce3d0d880e.js"
+  "/precache-manifest.0599a06029e387937abadb497dc763b8.js"
 );
 
 self.addEventListener('message', (event) => {
